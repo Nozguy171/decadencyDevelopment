c657413c33dfcbe6c005cf7d73f123f0553f483b
alfonso trejo <alfonso.trejo@uabc.edu.mx>
2024-09-17 18:54:35 -0700

    si

--- a.py
+++ a.py
@@ -5,7 +5,6 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv('API_KEY')
-genai.configure(api_key="AIzaSyBwMZ1rSnDQyQZJuXcFVDL8rT_v85WioRg")
 genai.configure(api_key=api_key) 
 
 app = Flask(__name__)
